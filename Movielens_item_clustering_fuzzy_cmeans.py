d68b68684dd921ee827e6df2524e5c9745471c1f
Shivani <shivaniszw_bt2k14@dtu.ac.in>
2018-04-22 08:50:52 +0530

    Refactor movielens fuzzy c means clustering code

--- Movielens_item_clustering_fuzzy_cmeans.py
+++ Movielens_item_clustering_fuzzy_cmeans.py
@@ -1,123 +1,164 @@
-from movielens import *
 import numpy as np
-from sklearn.metrics import mean_squared_error
-from sklearn.cluster import KMeans
-import sys
 import time
-import random
-import math
-import operator
 import pickle
+import skfuzzy as fuzz
+from movielens import *
+from sklearn.metrics import mean_squared_error
+
 user = []
 item = []
 rating = []
 rating_test = []
-import skfuzzy as fuzz
-import matplotlib.pyplot as plt
+avg_rating_per_cluster = []
+similarity_matrix = []
 
-d = Dataset()
-d.load_users("data/u10.user", user)
-d.load_items("data/u.item", item)
-d.load_ratings("data/u10.base", rating)
-d.load_ratings("data/u10.test", rating_test)
+n_users = 0
+n_items = 0
 
-n_users = len(user)
-n_items = len(item)
+# Load the movie lens dataset into arrays
+def load_data():
+    global user
+    global item
+    global rating
+    global rating_test
+    
+    d = Dataset()
+    d.load_users("data/u10.user", user)
+    d.load_items("data/u.item", item)
+    d.load_ratings("data/u10.base", rating)
+    d.load_ratings("data/u10.test", rating_test)
 
-utility = np.zeros((n_users, n_items))
-for r in rating:
-    utility[r.user_id-1][r.item_id-1] = r.rating
+def create_rating_matrix():
+    global n_users
+    global n_items
+
+    n_users = len(user)
+    n_items = len(item)
+    user_rating = np.zeros((n_users, n_items))
+    for r in rating:
+        user_rating[r.user_id-1][r.item_id-1] = r.rating
+
+    return user_rating
 
 # Finds the average rating for each user and stores it in the user's object
-for i in range(n_users):
-    rated = np.nonzero(utility[i]) #np.nonzero returns indices of the elements that are non-zero.
-    n = len(rated[0])
-    if n != 0:
-        user[i].avg_r = np.mean(utility[i][rated])
-    else:
-        user[i].avg_r = 0.
-
-# print (utility)
-test = np.zeros((n_users, n_items))
-for r in rating_test:
-    test[r.user_id - 1][r.item_id - 1] = r.rating
-movie_genre = []
-for movie in item:
-    movie_genre.append([movie.unknown, movie.action, movie.adventure, movie.animation, movie.childrens, movie.comedy,
-                        movie.crime, movie.documentary, movie.drama, movie.fantasy, movie.film_noir, movie.horror,
-                        movie.musical, movie.mystery, movie.romance, movie.sci_fi, movie.thriller, movie.war, movie.western])
-
-movie_genre = np.array(movie_genre)
-movie_genre = np.transpose(movie_genre)
-cntr, u_orig, _, _, _, _, _ = fuzz.cluster.cmeans(movie_genre, 19, 2, error=0.005, maxiter=300)
-labels = list(np.argmax(u_orig, axis=0) + 1)
-
-
-utility_clustered = []
-for i in range(0, n_users):
-    average = np.zeros(19)
-    tmp = []
-    for m in range(0, 19):
-        tmp.append([])
-    for j in range(0, n_items):
-        if utility[i][j] != 0:
-            tmp[labels[j] - 1].append(utility[i][j])#find the cluster of each movie the user has rated and append the rating
-    for m in range(0, 19):
-        if len(tmp[m]) != 0:
-            average[m] = np.mean(tmp[m])
+def find_avg_rating_per_user(user_rating):
+    for i in range(n_users):
+        rated = np.nonzero(user_rating[i]) #np.nonzero returns indices of the elements that are non-zero.
+        n = len(rated[0])
+        if n != 0:
+            user[i].avg_r = np.mean(user_rating[i][rated])
         else:
-            average[m] = 0
-    utility_clustered.append(average)
+            user[i].avg_r = 0.
 
-utility_clustered = np.array(utility_clustered)
-print (utility_clustered)
-for i in range(0, n_users):
-    x = utility_clustered[i]
-    user[i].avg_r = sum(a for a in x if a > 0) / sum(a > 0 for a in x)
+def cluster_movies():
+
+    movie_genre = []
+
+    for movie in item:
+        movie_genre.append([movie.unknown, movie.action, movie.adventure, movie.animation, movie.childrens, movie.comedy,
+                            movie.crime, movie.documentary, movie.drama, movie.fantasy, movie.film_noir, movie.horror,
+                            movie.musical, movie.mystery, movie.romance, movie.sci_fi, movie.thriller, movie.war, movie.western])
+
+    movie_genre = np.array(movie_genre)
+    movie_genre = np.transpose(movie_genre)
+    cntr, u_orig, _, _, _, _, _ = fuzz.cluster.cmeans(movie_genre, 19, 2, error=0.005, maxiter=300)
+    labels = list(np.argmax(u_orig, axis=0) + 1)
+
+    return labels
+
+def update_avg_rating(user_rating, labels):
+
+    global avg_rating_per_cluster
+
+    # find average rating of each movie cluster based on ratings of ith user for each movie in movie cluster
+    for i in range(0, n_users):
+        average = np.zeros(19)
+        tmp = []
+       
+        for m in range(0, 19):
+            tmp.append([])
+        
+        for j in range(0, n_items):
+            if user_rating[i][j] != 0:
+                tmp[labels[j] - 1].append(user_rating[i][j])#find the cluster of each movie the user has rated and append the rating
+        
+        for m in range(0, 19):
+            if len(tmp[m]) != 0:
+                average[m] = np.mean(tmp[m])
+            else:
+                average[m] = 0
+        
+        avg_rating_per_cluster.append(average)
+
+    avg_rating_per_cluster = np.array(avg_rating_per_cluster) #stores average rating of each cluster of all users
+    # print (avg_rating_per_cluster)
+
+    for i in range(0, n_users):
+        x = avg_rating_per_cluster[i]
+        user[i].avg_r = sum(a for a in x if a > 0) / sum(a > 0 for a in x)#final average rating of each user
 
 def pcs(x, y):
+    
+    global avg_rating_per_cluster
+
     num = 0
     den1 = 0
     den2 = 0
-    A = utility_clustered[x - 1]
-    B = utility_clustered[y - 1]
+    
+    A = avg_rating_per_cluster[x - 1]
+    B = avg_rating_per_cluster[y - 1]
+    
     num = sum((a - user[x - 1].avg_r) * (b - user[y - 1].avg_r) for a, b in zip(A, B) if a > 0 and b > 0)
+    
     den1 = sum((a - user[x - 1].avg_r) ** 2 for a in A if a > 0)
     den2 = sum((b - user[y - 1].avg_r) ** 2 for b in B if b > 0)
+    
     den = (den1 ** 0.5) * (den2 ** 0.5)
+    
     if den == 0:
         return 0
     else:
-        return num / den
+        return num/den
 
-pcs_matrix = np.zeros((n_users, n_users))
-for i in range(0, n_users):
-    for j in range(0, n_users):
-        if i!=j:
-            pcs_matrix[i][j] = pcs(i + 1, j + 1)
-            sys.stdout.write("\rGenerating Similarity Matrix [%d:%d] = %f" % (i+1, j+1, pcs_matrix[i][j]))
-            sys.stdout.flush()
-            time.sleep(0.00005)
-print ("\rGenerating Similarity Matrix [%d:%d] = %f" % (i+1, j+1, pcs_matrix[i][j]))
+def find_similarity_matrix():
+    pcs_matrix = np.zeros((n_users, n_users))#Pearson Correlation Similarity
 
+    for i in range(0, n_users):
+        for j in range(0, n_users):
+            if i!=j:
+                pcs_matrix[i][j] = pcs(i + 1, j + 1)
+                time.sleep(0.00005)
+
+    global similarity_matrix
+
+    similarity_matrix = pcs_matrix
+
+# Guesses the ratings that user with id, user_id, might give to item with id, i_id.
+# We will consider the top_n similar users to do this.
 def norm():
+
+    global avg_rating_per_cluster
+
     normalize = np.zeros((n_users, 19))
     for i in range(0, n_users):
         for j in range(0, 19):
-            if utility_clustered[i][j] != 0:
-                normalize[i][j] = utility_clustered[i][j] - user[i].avg_r
+            if avg_rating_per_cluster[i][j] != 0:
+                normalize[i][j] = avg_rating_per_cluster[i][j] - user[i].avg_r
             else:
                 normalize[i][j] = float('Inf')
     return normalize
 
 def guess(user_id, i_id, top_n):
+    
+    global similarity_matrix
+
     similarity = []
     for i in range(0, n_users):
         if i+1 != user_id:
-            similarity.append(pcs_matrix[user_id-1][i]) #append the row of user from similarity matrix
+            similarity.append(similarity_matrix[user_id-1][i]) #append the row of user from similarity matrix
     temp = norm()
     temp = np.delete(temp, user_id-1, 0)
-    top = [x for (y,x) in sorted(zip(similarity,temp), key=lambda pair: pair[0], reverse=True)]
+    top = [x for (y,x) in sorted(zip(similarity,temp), key=lambda pair: pair[0], reverse=True)] 
     s = 0
     c = 0
     for i in range(0, top_n):
@@ -132,25 +173,61 @@ def guess(user_id, i_id, top_n):
     else:
         return g
 
-utility_copy = np.copy(utility_clustered)
-for i in range(0, n_users):
-    for j in range(0, 19):
-        if utility_copy[i][j] == 0:
-            sys.stdout.write("\rGuessing [User:Rating] = [%d:%d]" % (i, j))
-            sys.stdout.flush()
-            time.sleep(0.00005)
-            utility_copy[i][j] = guess(i+1, j+1, 150)
-print ("\rGuessing [User:Rating] = [%d:%d]" % (i, j))
-pickle.dump( utility_copy, open("utility_matrix.pkl", "wb"))
-y_true = []
-y_pred = []
-f = open('test.txt', 'w')
-for i in range(0, n_users):
-    for j in range(0, n_items):
-        if test[i][j] > 0:
-            f.write("%d, %d, %.4f\n" % (i+1, j+1, utility_copy[i][labels[j]-1]))
-            y_true.append(test[i][j])
-            y_pred.append(utility_copy[i][labels[j]-1])
-f.close()
+def predict_user_rating():
 
-print ("Mean Squared Error: %f" % mean_squared_error(y_true, y_pred))
+    global avg_rating_per_cluster
+
+    user_rating_copy = np.copy(avg_rating_per_cluster)
+    
+    for i in range(0, n_users):
+        for j in range(0, 19):
+            if user_rating_copy[i][j] == 0:
+                time.sleep(0.00005)
+                user_rating_copy[i][j] = guess(i+1, j+1, 150)
+
+    pickle.dump( user_rating_copy, open("user_rating_matrix.pkl", "wb"))
+    
+    return user_rating_copy
+
+def create_test_matrix():
+    test = np.zeros((n_users, n_items))
+
+    for r in rating_test:
+        test[r.user_id - 1][r.item_id - 1] = r.rating
+
+    return test
+
+def calculate_error(test, predicted_rating,cluster):
+    # Predict ratings for u.test and find the mean squared error
+    y_true = []
+    y_pred = []
+    
+    f = open('test.txt', 'w')
+    
+    for i in range(0, n_users):
+        for j in range(0, n_items):
+            if test[i][j] > 0:
+                f.write("%d, %d, %.4f\n" % (i+1, j+1, predicted_rating[i][labels[j]-1]))
+                y_true.append(test[i][j])
+                y_pred.append(predicted_rating[i][labels[j]-1])
+    f.close()
+
+    print ("Mean Squared Error: %f" % mean_squared_error(y_true, y_pred))
+
+def test_model(predicted_rating,cluster):
+    test_matrix = create_test_matrix()
+    calculate_error(test_matrix, predicted_rating, cluster)
+
+def main():
+    load_data()
+    user_rating = []
+    user_rating = create_rating_matrix()
+    find_avg_rating_per_user(user_rating)
+    cluster = cluster_movies()
+    update_avg_rating(user_rating, cluster)
+    find_similarity_matrix()
+    predicted_rating = predict_user_rating()
+    test_model(predicted_rating,cluster)
+
+if __name__ == '__main__':
+    main()
