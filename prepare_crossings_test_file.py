279c8bf9c788419dfb7f69d0e15b316be5e1c214
Shivani <shivaniszw_bt2k14@dtu.ac.in>
2018-04-27 07:32:40 +0530

    Replace user and book IDs with their indices

--- prepare_crossings_test_file.py
+++ prepare_crossings_test_file.py
@@ -10,6 +10,36 @@ with open('data/data_csv/book_crossings/BX-Book-Ratings.csv', 'r') as csvfile, o
         if row['Book-Rating'] != '0':
             writer.writerow({'User-ID': row['User-ID'], 'ISBN': row['ISBN'], 'Book-Rating': row['Book-Rating']})
 
+with open('data/data_csv/book_crossings/BX-Book-Explicit-Ratings.csv', 'r') as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+
+    user_id, book_id = set(), set()
+
+    for row in reader:
+        user_id.add(row['User-ID'])
+        book_id.add(row['ISBN'])
+
+user_id = list(user_id)
+book_id = list(book_id)
+
+print len(book_id)
+
+with open('data/data_csv/book_crossings/BX-Book-Explicit-Ratings.csv', 'r') as csvfile, open('data/data_csv/book_crossings/BX-Book-Explicit-Ratings-Updated.csv', 'w') as outputfile:
+  
+    reader = csv.DictReader(csvfile, fieldnames=fieldnames)
+    writer = csv.DictWriter(outputfile, fieldnames=fieldnames)
+
+    for row in reader:
+        
+        if(row['User-ID'] != 'User-ID'):
+            
+            row['User-ID'] = user_id.index(row['User-ID'])
+            
+            row['ISBN'] = book_id.index(row['ISBN'])
+
+            writer.writerow({'User-ID': row['User-ID'], 'ISBN': row['ISBN'], 'Book-Rating': row['Book-Rating']})
+
+shutil.move('data/data_csv/book_crossings/BX-Book-Explicit-Ratings-Updated.csv', 'data/data_csv/book_crossings/BX-Book-Explicit-Ratings.csv')
 
 f = open('data/data_csv/book_crossings/BX-Book-Explicit-Ratings.csv','r')
 o = open('data/data_csv/book_crossings/BX-Book-Test-Ratings.csv', 'w')
